7858614e94dee5665bd792969c5dbe5c9f03577f
Gareth Griffiths <gareth.griffiths2@dvla.gov.uk>
2021-02-04 11:47:01 +0000

    parallel_report_portal.rb - Amended the ENV var naming.

--- lib/parallel_report_portal.rb
+++ lib/parallel_report_portal.rb
@@ -25,12 +25,12 @@ module ParallelReportPortal
   def wait_while_processes_finish
     return unless ENV["TEST_ENV_NUMBER"]
     # ENV['RP_PROC_WAIT_TO_CLOSE_RETRY_LIMIT'] is the break out limit of retries before closing down
-    exit_limit = ENV['RP_PROC_WAIT_TO_CLOSE_RETRY_LIMIT'] || 3
+    exit_limit = ENV['RP_PROC_WAIT_TO_EXIT_RETRY_LIMIT'] || 3
     exit_counter = 0
     # ENV['RP_PROC_WAIT_TO_CLOSE_SLEEP'] Is the initial sleep in the loop to retry
     # and check if the processes have completed, this will be incremented by 30 secs
     # every loop.
-    initial_wait_time = ENV['RP_PROC_WAIT_TO_CLOSE_SLEEP'] || 30
+    initial_wait_time = ENV['RP_PROC_WAIT_TO_EXIT_SLEEP'] || 30
     loop do
       if ParallelTests.number_of_running_processes <= 1
         break
